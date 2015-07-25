a08b9bc8c183273106ffa285dd263d9b39816f11
MaggieMoss <maggiebmoss@gmail.com>
2015-07-25 16:40:57 -0700

    WPI - pushing before pairing

--- LipstickProject/app/models/donation.rb
+++ LipstickProject/app/models/donation.rb
@@ -1,2 +1,6 @@
 class Donation < ActiveRecord::Base
+	belongs_to :user
+	validates :amount, :presence => true
+	validates :amount, numericality: true
+	validates :amount, numericality: { greater_than: 0}
 end
--- LipstickProject/app/models/user.rb
+++ LipstickProject/app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :addresses
 	has_many :donations
-	
-	validates :email, presence :true
+
+	validates :email, :presence => true
 	validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
 end
 
