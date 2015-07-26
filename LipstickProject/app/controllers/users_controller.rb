e713a19daef1e03f04336879f1c0ff6c094b92e3
Isha <ishak@isha.local>
2015-07-25 18:06:17 -0700

    Properly persist nested associations

--- LipstickProject/app/controllers/users_controller.rb
+++ LipstickProject/app/controllers/users_controller.rb
@@ -6,13 +6,43 @@ class UsersController < ApplicationController
 	end 
 
   def create
-    @user = User.new(params[:user])
+    address = Address.new(user_params[:addresses])
+    donation = Donation.new(user_params[:donations])
 
+    @user = User.new(user_params.except(:addresses, :donations))
+    @user.addresses << address
+    @user.donations << donation
+    
     @user.save
     redirect_to @user
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :first_name, 
+      :last_name, 
+      :email, 
+      :accepts_marketing,
+      :recurring,
+      addresses: [
+        :street,
+        :house,
+        :postal_code,
+        :city,
+        :province
+      ],
+      donations: [
+        :amount
+      ]
+    )
   end
 end
--- LipstickProject/app/models/user.rb
+++ LipstickProject/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 	has_many :addresses
 	has_many :donations
 
+	accepts_nested_attributes_for :addresses, :donations
+
 	validates :email, :presence => true
 	validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
