335aa133fe468981365732e4a0abcf9845c0421e
MaggieMoss <maggiebmoss@gmail.com>
2015-07-26 16:17:11 -0700

    fixed CAD movement on mobile view

--- app/assets/javascripts/psuedo.js
+++ app/assets/javascripts/psuedo.js
@@ -1,5 +1,4 @@
 $(document).on('ready', function() {
-	$('.donation-box').after('<p style="position: absolute;top: 37%;right: 18%;opacity:0.5"> CAD </p>')
 
 	$('.collapsible-header').on('click', function() {
 		var sibling_row_one = $(this).next();
