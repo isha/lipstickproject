3b67cb4333c2f32a018945a90696a062ba9d08a5
Isha <ishak@isha.local>
2015-07-26 13:27:28 -0700

    Uncomment stripe code in controller that i commneted earlier

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,21 +15,21 @@ class UsersController < ApplicationController
 
     if @user.save
       Resque.enqueue(SendTaxReceiptEmailJob, @user.donations.last.id)
-      # if(!@user.recurring)
-      #   Stripe.api_key = "sk_test_47WSgDMSGAE8OrlTNbQQHAG4"
-      #   token = params[:stripeToken]
-      #   # if payment is one-time
-      #   begin
-      #     charge = Stripe::Charge.create(
-      #       :amount => 1000, #reminder: amount in cents
-      #       :currency => 'cad', 
-      #       :source => token, 
-      #       :description => "Example charge"
-      #     )
-      #   rescue Stripe::CardError => e
-      #     # the card has been declined
-      #   end 
-      # end 
+      if(!@user.recurring)
+        Stripe.api_key = "sk_test_47WSgDMSGAE8OrlTNbQQHAG4"
+        token = params[:stripeToken]
+        # if payment is one-time
+        begin
+          charge = Stripe::Charge.create(
+            :amount => 1000, #reminder: amount in cents
+            :currency => 'cad', 
+            :source => token, 
+            :description => "Example charge"
+          )
+        rescue Stripe::CardError => e
+          # the card has been declined
+        end 
+      end 
       redirect_to @user
     else
       render :new
