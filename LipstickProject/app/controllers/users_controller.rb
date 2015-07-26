ec04f814d2814147a174318bca4a543e086c5f9a
Isha <ishak@isha.local>
2015-07-25 17:01:52 -0700

    WIP donations & users persisting & showing

--- LipstickProject/app/controllers/users_controller.rb
+++ LipstickProject/app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
 	def new 
 		@user = User.new
-		1.times { @user.addresses.build }
+		@user.addresses.build
+		@user.donations.build
 	end 
+
+  def create
+    @user = User.new(params[:user])
+
+    @user.save
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
