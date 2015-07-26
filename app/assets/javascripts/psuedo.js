f677e02855b3463bdab05bb39a83181a23914c2d
MaggieMoss <maggiebmoss@gmail.com>
2015-07-26 16:30:01 -0700

    refatored how the table works

--- app/assets/javascripts/psuedo.js
+++ app/assets/javascripts/psuedo.js
@@ -1,14 +1,11 @@
 $(document).on('ready', function() {
 
 	$('.collapsible-header').on('click', function() {
-		var sibling_row_one = $(this).next();
-		var sibling_row_two = $(this).next().next();
-		if(sibling_row_one.hasClass('show')){
-			sibling_row_one.removeClass('show');
-			sibling_row_two.removeClass('show');
+		var class_selector = 'user-'+ $(this).attr('id');
+		if($('.'+class_selector).hasClass('show')){
+			$('.'+class_selector).removeClass('show');
 		} else {
-			sibling_row_two.addClass('show');
-			sibling_row_one.addClass('show');
+			$('.'+class_selector).addClass('show');
 		}
 	})
 })
