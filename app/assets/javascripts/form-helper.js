9f4e030da664d6724c1b91fff52e4d6cee3065c9
MaggieMoss <maggiebmoss@gmail.com>
2015-11-21 15:49:50 -0500

    back button on both views

--- app/assets/javascripts/form-helper.js
+++ app/assets/javascripts/form-helper.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
 		$('.second_form_view').css('display', 'block');
 		var amount = $('.donation-box').val();
 		$('.final-amount').val(amount);
+		$('.back-button').css('display', 'block');
+
 	});
 	// make the monthly donation form appear on button click.
 	$('.sub-initial-monthly').on('click', function(e){
@@ -29,7 +31,6 @@ $(document).ready(function(){
 		$('.third_form_view').css('display', 'none');
 		$('.second_form_view').css('display', 'none');
 		$('.back-button').css('display', 'none');
-
 	});
 
 
